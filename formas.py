e5f50fc60dfa3c15f4a67e31c6b3a3b76764dd43
SÉRGIO DOMINGOS DA SILVA JUNIOR <26210943@dsn127m1246728.cfp127aluno.local>
2026-09-18 14:55:26 -0300

    calculo de circulo, Triângulo, Quadrado e Retângulo.

---
+++ formas.py
@@ -0,0 +1,53 @@
+
+# 4 - retangulo
+# 5 - paralelogramo
+# 6 - losango
+# 7 - trapezios
+# python3 formas.py
+
+#   print ("4 - Retângulo")
+#   print ("5 - Paralelogramo")
+#   print ("6 - Losango")
+#   print ("7 - Trapézios")
+#   print ("0 - Sair")
+
+def Circulo():
+    raio = int (input ("qual o Raio do circulo? "))
+    raio = raio * raio
+    print (f"A área do circulo é: {raio * 3.14}")
+def Triângulo():
+    base = int (input ("qual a base do Triângulo? "))
+    altura = int (input ("qual a altura do Trângulo? "))
+    calculo = base * altura
+    print (f"A área do Triângulo é: {calculo / 2}")
+def Quadrado():
+    lado = int (input ("qual o valor de um dos lados do Quadrado? "))
+    print (f"A área do Quadrado é: {lado * lado}")
+def Retângulo():
+    base = int (input ("qual a base do Retângulo? "))
+    altura = int (input ("qual a altura do Retângulo? "))
+    print (f"A área do Retângulo é: {base * altura}")
+
+
+
+while True: 
+    print ("CALCULADORA")
+    print ("1 - Circulo")
+    print ("2 - Triângulo")
+    print ("3 - Quadrado")
+    print ("4 - Retângulo")
+    print ("0 - Sair")
+
+    opcao = input ("Escolha uma opção: ")
+
+    if opcao == "1":
+        Circulo()
+    elif opcao == "2":
+        Triângulo()
+    elif opcao == "3":
+        Quadrado()
+    elif opcao == "4":
+        Retângulo()
+    elif opcao == "0":
+        print ("Saindo do sistema...")
+        break
